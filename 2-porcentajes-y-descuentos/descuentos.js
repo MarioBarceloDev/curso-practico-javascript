1b6b7ef49da7da5d91ae78b496a6183d687601c4
Mario Barceló <mariobarcelosospedra@gmail.com>
2021-07-22 10:51:43 +0200

    New discounts file with coupons and DOM interaction for the simple discount calculator

--- 2-porcentajes-y-descuentos/descuentos.js
+++ 2-porcentajes-y-descuentos/descuentos.js
@@ -1,44 +1,32 @@
-// COUPON DISCOUNTS
 
-// Coupon Array
-let coupons = [
-    {
-        name: 'CUPON25',
-        value: 25,
-    },
-    {
-        name: 'CUPON50',
-        value: 50,
-    },
-];
 
-// Function to know the price with coupon
-function finalPrice(productPrice, coupon) {
-    // If there is NOT a coupon
-    if (!coupon) {
-        return `You have not entered a coupon. The price of the product is ${productPrice}`;
+// Function to know the price with a discount
+function finalPrice(productPrice, discount) {
+    // If there is NOT a discount
+    if (!discount) {
+      
+        return `You have not entered a discount. The price of the product is ${productPrice}`;
     }
 
-    // If there is a coupon
-    if (coupon) {
-        // Looking for the coupon in the array
-        let couponObject = coupons.find((item) => item.name === coupon);
+    // If there is a discount
+    if (discount) {
 
-        // If there is a valid coupon
-        if (couponObject !== undefined) {
-            let couponName = couponObject.name;
-            let couponValue = couponObject.value;
+            let discountedPrice = (productPrice * (100 - discount)) / 100;
 
-            let discountedPrice = (productPrice * (100 - couponValue)) / 100;
-
-            return `Your product with price ${productPrice} and percentage discount of ${couponValue}% has a final value of ${discountedPrice}.`;
+            return `Your product with price ${productPrice} and a discount of ${discount}% has a final value of ${discountedPrice}.`;
         }
-
-        // If there is NOT a valid coupon
-        if (couponObject === undefined) {
-            return `The coupon you have entered is invalid. Please enter a valid one. The value of your product is ${productPrice}`;
-        }
-    }
 }
 
-// finalPrice(100, 'CUPON50');
+
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const priceWithDiscount = finalPrice(priceValue, discountValue);
+
+    const resultP = document.getElementById("resultP");
+    resultP.innerText = priceWithDiscount;
+}
